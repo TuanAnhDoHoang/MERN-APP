b8c60636bdcfc3e1f10bfd0ad0dfd7341a4703c5
anhdoo rustyCrab <anhdoo1211@gmail.com>
2025-02-22 09:11:51 +0700

    blog init

---
+++ blog/client/my-app/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="header">
+        Blog
+      </div>
+      <div className="blogs">
+        <div className="info">
+          <img src="" alt="" />
+          <span></span>
+        </div>
+        <img src="" alt="" />
+        <h3 className="title"></h3>
+        <p className="content"></p>
+        <div className="emoji">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
